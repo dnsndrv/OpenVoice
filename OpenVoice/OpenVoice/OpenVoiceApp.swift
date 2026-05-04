dccc2acf9f2f623bad031ae62f178064b89ca57c
dev <dev@local>
2026-05-04 16:22:09 +0300

    chore: import Xcode template skeleton

---
+++ OpenVoice/OpenVoice/OpenVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenVoiceApp.swift
+//  OpenVoice
+//
+//  Created by Denis Andreev on 04.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenVoice/OpenVoiceTests/OpenVoiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenVoiceTests.swift
+//  OpenVoiceTests
+//
+//  Created by Denis Andreev on 04.05.2026.
+//
+
+import Testing
+@testable import OpenVoice
+
+struct OpenVoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
